12068e5d6cf591ec17f4269f2f44468cf53ed26f
Emil <mawlitov.e@yandex.ru>
2022-12-02 14:50:38 +0400

    sdelal

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+double number = Convert.ToInt32(Console.ReadLine());
+
+if(number % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число не четное");
+}
